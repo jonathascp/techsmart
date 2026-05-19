0c0859a40c3600a884b0810fe38a86112c178ea4
Jonathas dos Anjos <jonathas_rj_10@hotmail.com>
2026-05-19 20:21:21 -0300

    Atualizaçao do menu com animacao

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-// const btn = document.querySelector(".menu-btn");
-// const menu = document.querySelector(".menu");
+const btn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const header = document.querySelector("header");
+const lista = document.querySelectorAll(".list-menu a");
 
-// btn.addEventListener("click", () => {
-//   menu.classList.toggle("active");
-// });
+lista.forEach(item => {
+    item.addEventListener("click", () => {
+        if(menu.classList.contains("active"))
+        {
+            menu.classList.remove("active");
+        }
+    } )
+})
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 100)
+    {
+        header.classList.add("fixed");
+    }
+    else
+    {
+        header.classList.remove("fixed");
+    }
+})
+
